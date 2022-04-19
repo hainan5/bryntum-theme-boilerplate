3b46c38254939d77c17b5fa9391e38218b31603f
hainan5 <earthyan5@gmail.com>
2022-04-19 15:44:48 +0800

    update:更新ignore文件

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect, useRef } from "react";
+import { BryntumGrid } from "@bryntum/grid-react";
+import { DomHelper } from "@bryntum/grid/grid.umd";
+
+const dataGenerate = (number) => {
+  const data = [];
+  const names = [
+    "Dom Taylor",
+    "John Adams",
+    "Maxim Gagarin",
+    "Linda Johansson",
+    "Mike Thomas",
+  ];
+  const city = ["Paris", "Dubai", "Los Angeles", "New York", "Moscow"];
+  for (let i = 0; i < number; i++) {
+    data.push({ id: i + 1, name: names[i % 5], city: city[i % 5], age: 24 });
+  }
+  return data;
+};
+
+const App = (props) => {
+  const [readOnly, setReadOnly] = useState(true);
+  const ref = useRef(null);
+  const gridConfig = {
+    // appendTo:'root',
+    // width: 600,
+    cellMenu: true,
+
+    headerMenu: true,
+    features: {
+      rowReorder: {
+        showGrip: true,
+        listeners: {
+          gridRowBeforeDropFinalize: async ({ context }) => {
+            console.log("拖动成功", context);
+          },
+        },
+      },
+      // cellEdit: true,
+      sort: true,
+      filter: true,
+      cellEdit: {
+        triggerEvent: "cellclick",
+        // finishCellEdit: async ({ context }) => {
+        //   console.log("123");
+        // },
+      },
+    },
+
+    showDirty: true, // 展示脏数据
+    readOnly,
+    columns: [
+      // start new
+      { type: "rownumber", width: 100, align: "center" },
+      { field: "name", text: "Name", flex: 1 },
+      { field: "city", text: "City", flex: 1 },
+      {
+        type: "number",
+        field: "age",
+        text: "Age",
+        flex: 1,
+        htmlEncode: false,
+        renderer: ({ value }) => {
+          if (value > 30) {
+            return `<span style="color: red; font-weight: bold">${value}</span>`;
+          }
+          return value;
+        },
+      },
+    ],
+
+    data: dataGenerate(10),
+
+    tbar: [
+      {
+        type: "buttonGroup",
+        items: [
+          {
+            ref: "material",
+            text:'material',
+            onClick: ({ source }) => {
+              DomHelper.setTheme("material");
+              // document.getElementById("bryntum-theme").setAttribute("href","http://localhost:3000/themes/grid.material.css");
+            },
+            cls:"b-raised"
+          },
+          {
+            ref: "classic-dark",
+            text: "classic-dark",
+            onClick: ({ source }) => {
+              DomHelper.setTheme("classic-dark");
+              // document.getElementById("bryntum-theme").setAttribute("href","http://localhost:3000/themes/grid.classic-dark.css");
+            },
+          },
+          {
+            ref: "classic-light",
+            text:'classic-light',
+            onClick: ({ source }) => {
+              DomHelper.setTheme("classic-light");
+              // document.getElementById("bryntum-theme").setAttribute("href","http://localhost:3000/themes/grid.classic-light.css");
+            },
+            cls:"b-raised"
+          },
+          {
+            ref: "classic",
+            text: "classic",
+            onClick: ({ source }) => {
+              DomHelper.setTheme("classic");
+              // document.getElementById("bryntum-theme").setAttribute("href","http://localhost:3000/themes/grid.classic.css");
+            },
+          },
+          {
+            ref: "custom",
+            text: "custom",
+            onClick: ({ source }) => {
+              DomHelper.setTheme("custom");
+              // document.getElementById("bryntum-theme").setAttribute("href","http://localhost:3000/themes/grid.custom.css");
+            },
+            cls:"b-raised"
+          },
+        ],
+      },
+    ],
+  };
+
+  useEffect(() => {
+    // DomHelper.setTheme("classic-dark");
+  }, []);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <BryntumGrid ref={ref} {...gridConfig} />
+    </div>
+  );
+};
+
+export default App;
